de9fedb1610c2ea0f3c2bf96874546bd27824298
oyugirachel <oyugirachel20@gmail.com>
2020-09-24 16:49:31 +0300

    Added The recycler viewer and adapter to display the posts and comments Respectively

--- app/src/main/java/com/example/postsapp/viewmodel/CommentsViewModelFactory.kt
+++ app/src/main/java/com/example/postsapp/viewmodel/CommentsViewModelFactory.kt
@@ -6,7 +6,7 @@ import com.example.postsapp.repository.CommentsRepository
 
 class CommentsViewModelFactory(val commentsRepository: CommentsRepository):ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        if(modelClass.isAssignableFrom(PostsViewModel::class.java)){
+        if(modelClass.isAssignableFrom(CommentsViewModel::class.java)){
             return CommentsViewModel(commentsRepository)as T
         }
         throw IllegalArgumentException("Unknown viewmodel class")
